67facb65d683881990c15a093e729e40492f9b91
cywtest <cyw_work_8212@163.com>
2023-06-16 18:08:25 +0800

    6.16使用混杂设备的简化代码并完善按键读入的功能

--- gpio_keys_agn_1.c
+++ gpio_keys_agn_1.c
@@ -13,6 +13,8 @@
 #include <linux/of_fdt.h>
 #include <linux/of_platform.h> 
 
+#include <linux/miscdevice.h> //混杂设备注册
+
 #define GPIO_MAX_COUNT    保存最大引脚数
 //保存引脚的数组
 static struct gpio key_gpios[GPIO_MAX_COUNT];
@@ -27,69 +29,11 @@ static int key_major = 0;
 #define KEY_NAME            "key"           /* 按键名称 */
 
 
-/* key设备结构体 */
-struct key_dev {
-    dev_t devid;                /* 设备号 */
-    struct cdev chrdev;         /* cdev */
-    struct class *devclass;     /* 设备类指针 */
-    struct device *device;      /* 设备指针 */
-    int major;                  /* 主设备号 */
-    int minor;                  /* 次设备号 */
-    struct device_node *nd;     /* 设备节点 */
-    int key_gpio;               /* key使用的GPIO编号 */
-    atomic_t key_val;           /* 按键值 */
+/* key结构体 */
+struct key_data {
+    int gpio;
 };
-//实例化
-struct key_dev keydev;
-
-//按键io口初始化
-static int key_io_init(struct device_node *pdevnode)
-{
-	int rt;
-	int count, index;
-	unsigned int pins[GPIO_MAX_COUNT];	
-	const char *str;
-	
-	//从设备树中提取GPIO口信息 
-	/*第一个参数是设备节点 
-	第二个参数要读取的属性名字 
-	第三个参数是数组  
-	第四个参数数组元素个数
-	*/
-	rt = of_property_read_u32_array(pdevnode, "key-gpios", pins, count);
-	
-	if(rt < 0)
-	{
-		printk(KERN_ERR"of_property_read_u32_array key-gpios fail\n");	
-		goto err_of_property_read_u32_array;	
-	}
-    //统计引脚个数
-	for (index = 0; index < count; index++) 
-	{
-		key_gpios[index].gpio =  pins[index];
-		
-		printk(KERN_INFO"正确获取pin %d\n",pins[index]);
-	}
-	
-   
-	//初始化key所用的IO口 
-	gpio_free_array(key_gpios, count);
-	//申请一组gpio
-	rt = gpio_request_array(key_gpios,count);
-	//设置端口为输入 
-    gpio_direction_input(key_gpios[0]);
-	gpio_direction_input(key_gpios[1]);
-	if(rt < 0)
-	{
-		printk(KERN_ERR"gpio_request_array error\n");
-		goto err_gpio_request_array;
-	}
-	
-	//错误跳转
-	err_of_property_read_u32_array:
-	err_gpio_request_array:
-    return 0;
-}
+//实例化 改在probe里面分配空间方便释放
 
 //打开开关函数
 static int key_open(struct inode *node, struct file *filp)
@@ -115,42 +59,22 @@ static int key_close (struct inode *inode, struct file *file)
 static ssize_t key_read(struct file *filp, char __user *buf, size_t len, 
 loff_t *fpos)
 {
-    int ret = 0;
+    int value;
 	//保存读取引脚的值
-    char key_val[2];
-	//私有数据
-	struct key_dev *dev = filp->private_data;
-	key_val[0]=key_read_handle(key_gpios[0])
-	key_val[1]=key_read_handle(key_gpios[1])
+	//获取设备私有数据结构体所在的设备结构体的指针。
+	struct key_data *data = container_of(filp->private_data,
+                                        struct my_key, miscdev);
+
 	//输出数据
-    ret = copy_to_user(buf, &key_val, sizeof(key_val));
+    value = gpio_get_value(data->gpio);
 
-    return ret;
+    if (copy_to_user(buf, &value, sizeof(value)))
+        return -EFAULT;
+
+    return sizeof(value);
+
 }
-//按键处理函数
-char key_read_handle(static struct gpio key_gpios)
-{
-	char key_val;
-    if (gpio_get_value(key_gpios) == 0 |) {
-        /* key按下 */
 
-        /* 等待按键释放 */
-        while(gpio_get_value(key_gpios) == 0);
-
-        /* 设置按键值 
-		将原子变量设置为KEY0_VAL  0xF0 证明按下了按键
-		*/
-        atomic_set(&dev->key_val, KEY0_VAL);
-    } else {
-        //设置按键值 没有按下设置为0
-        atomic_set(&dev->key_val, INV_KEY_VAL);
-    }
-
-    /* 保存按键值 */
-    key_val=atomic_read(&dev->key_val);
-	return key_val;
-	
-}
 //设备操作函数 
 static struct file_operations key_fops = {
     .owner = THIS_MODULE,
@@ -158,89 +82,100 @@ static struct file_operations key_fops = {
 	.release = key_close,
     .read = key_read,
 };
+//混杂设备的结构体
+static struct miscdevice key_miscdev[] = {
+    {
+        .minor = MISC_DYNAMIC_MINOR,
+        .name = "my-key0",
+        .fops = &key_fops,
+    },
+    {
+        .minor = MISC_DYNAMIC_MINOR,
+        .name = "my-key1",
+        .fops = &key_fops,
+    },
+};
+
+
+
 static int mykey_probe(struct platform_device *pdev)
 {
 	//初始化
-	struct device_node *pdevnode = pdev->dev.of_node;
-	key_io_init(pdevnode);
-	
-	//注册字符设备驱动 创建设备号 */
-    if (keydev.major) {
-        /* 已经定义主设置号 根据主设备号申请设备ID */
-        keydev.devid = MKDEV(keydev.major, 0);
+	struct device *dev = &pdev->dev;
+	struct key_data *data;
+	int i, ret;
+	//遍历获取gpio口的值
+	for (i = 0; i < ARRAY_SIZE(key_miscdev); i++) {
+		//这样分配的空间设备被卸载时会自动释放
+        data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+        if (!data)
+            return -ENOMEM;
 
-	 //注册字符设备 
-        register_chrdev_region(keydev.devid, KEY_CNT, KEY_NAME);
-    } else {
-		
-    //未定义主设备号,让内核动态分配设备ID *	
-	//第一个参数：获取系统分配的设备号
-	//第二个参数：次设备号的开始值
-	//第三个参数：次设备的数量为1
-	//第四个参数：若设备号有效，则在/proc/devices文件中找到"key"/
-        alloc_chrdev_region(&keydev.devid, 0, KEY_CNT, KEY_NAME);
+        data->gpio = of_get_gpio(dev->of_node, i);
+        if (data->gpio < 0) {
+            dev_err(dev, "failed to get gpio%d\n", i);
+            return data->gpio;
+        }
+		//读值
+        ret = gpio_request(data->gpio, "agn,gpio_key");
+        if (ret) {
+            dev_err(dev, "failed to request gpio%d\n", i);
+            return ret;
+        }
+		//设置输入
+        ret = gpio_direction_input(data->gpio);
+        if (ret) {
+            dev_err(dev, "failed to set gpio%d direction\n", i);
+            goto err_gpio;
+        }
 
-        /* 获取主设备号 */
-        keydev.major = MAJOR(keydev.devid);
+        key_miscdev[i].name = devm_kasprintf(dev, GFP_KERNEL, "%s%d",
+                                                   KEY_NAME, i);
+        if (!key_miscdev[i].name) {
+            ret = -ENOMEM;
+            goto err_gpio;
+        }
 
-        /* 获取次设备号 */
-        keydev.minor = MINOR(keydev.devid);
+        key_miscdev[i].parent = dev;
+        key_miscdev[i].minor = MISC_DYNAMIC_MINOR;
+        key_miscdev[i].mode = S_IRUSR | S_IWUSR;
+        key_miscdev[i].private_data = data;
+
+        ret = misc_register(&key_miscdev[i]);
+        if (ret) {
+            dev_err(dev, "failed to register misc device %d\n", i);
+            goto err_gpio;
+        }
+
+        platform_set_drvdata(pdev, data);
     }
 
-    /* 初始化cdev成员 */
-    keydev.chrdev.owner = THIS_MODULE;
-	cdev_init(&keydev.chrdev, &key_fops);
-
-    /* 添加一个cdev */
-    cdev_add(&keydev.chrdev, keydev.devid, KEY_CNT);
-	
-	/* 创建类 */
-    keydev.devclass = class_create(THIS_MODULE, KEY_NAME);
-	// IS_ERR()就是用来判断指针是否有错
-    if (IS_ERR(keydev.devclass)) {
-        return PTR_ERR(keydev.devclass);
-    }
-
-    /* 创建设备 */
-    keydev.device = device_create(keydev.devclass, NULL, keydev.devid, NULL, KEY_NAME);
-    if (IS_ERR(keydev.device)) {
-        return PTR_ERR(keydev.device);
-    }
+    pr_info(DRIVER_NAME ": initialized\n");
 
     return 0;
-		
-		
-		
+
+err_gpio:
+while (--i >= 0) {
+	data = platform_get_drvdata(pdev);
+	misc_deregister(&key_miscdev[i]);
+	gpio_free(data->gpio);
+}
+return ret;	
 		
 }
 		
 static int mykey_remove(struct platform_device *dev)
 {	
-	int count;
-	struct device_node *pdevnode = dev->dev.of_node;
-	printk(KERN_INFO "mykey: key platform driver remove!\r\n");
-	atomic_set(&keydev.key_val, INV_KEY_VAL);
+    struct key_data *data;
+    int i;
 
+    for (i = 0; i < ARRAY_SIZE(key_miscdev); i++) {
+        data = platform_get_drvdata(pdev);
+        misc_deregister(&key_miscdev[i]);
+        gpio_free(data->gpio);
+    }
 
-	/* 计数 */
-    count = sizeof(key_gpios)/sizeof(key_gpios[0]);
-	
-	/* 删除cdev */
-	cdev_del(&keydev.chrdev);
-
-	/* 注销设备号 */
-	unregister_chrdev_region(keydev.devid, KEY_CNT);
-	
-	/* 注销设备 */
-	device_destroy(keydev.devclass, keydev.devid);
-
-	/* 注销设备类 */
-	class_destroy(keydev.devclass);
-
-	//释放引脚
-	gpio_free_array(key_gpios, count);
-
-	return 0;
+    return 0;
 }
 
 
@@ -253,12 +188,17 @@ static const struct of_device_id key_of_match[] = {
 	{ .compatible = "agn,gpio_key" },//兼容属性与设备树上的一致
 	{ /* Sentinel 最后一个要空的*/ }
 };
+//
+定义设备ID列表，以便内核可以自动将驱动程序与设备进行�
+�配
+MODULE_DEVICE_TABLE(of, key_of_match);
 
 /* platform驱动结构体 */
 static struct platform_driver key_driver = {
 	.driver = {
-		.name			= "mykey",		// 驱动名字，用于和设备匹配
-		.of_match_table	= key_of_match,		// 设备树匹配表，用于和设备树中定义的设备匹配
+		.name			= KEY_NAME,		// 驱动名字，用于和设备匹配
+		.of_match_table	= key_of_match,		// 
+设备树匹配表，用于和设备树中定义的设备匹配
 	},
 	.probe		= mykey_probe,	// probe函数
 	.remove		= mykey_remove,	// remove函数
@@ -288,7 +228,8 @@ static void __exit _key_exit(void)
 	/* 平台解绑 */
 	platform_driver_unregister(&key_driver);
 }
-
+//也可以将该平台设备驱动程序注册到内核中
+//module_platform_driver(key_driver);
 
 
 //驱动程序的入口
